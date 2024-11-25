820b1324809563ad1b13dee5329a53a7928ae8a5
Mohit-Kushwah <83336428+MohitKushwah-coder@users.noreply.github.com>
2024-11-25 15:30:03 +0530

    Update Multiple_Disease_Prediction_Web_App.py

--- Multiple_Disease_Prediction_Web_App.py
+++ Multiple_Disease_Prediction_Web_App.py
@@ -147,7 +147,7 @@ if (selected == 'Brain Stroke Prediction'):
     with col2:
         ever_married = st.text_input('Ever Married (1 = Yes, 0 = No)')
     with col3:
-        work_type = st.text_input('Work Type (0 = Gov-Job, 1 = Never-Worked, 2 = Private, 3 = Self-Emp, 4 = Children)') 
+        work_type = st.text_input('Work Type (0 = Gov, 1 = Never-Worked, 2 = Private, 3 = Self, 4 = Child)') 
     with col1:
         Residence_type = st.text_input('Residence Type (1 = Urban, 0 = Rural)')
     with col2:
